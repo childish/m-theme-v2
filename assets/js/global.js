5452ce096a0b940e5e796049f88793c83827d6f0
Mackenzie Child <mackenzie.child@gmail.com>
2014-05-12 16:10:55 -0600

    Add progress indicator to blog posts

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,60 +1,47 @@
-$(".main_content_nav_m").addClass("hide");
-$(document).ready(function() {
+$(document).ready(function(){
 
-	// Google Analytics 
-	if(location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
+		// By Pankaj Parashar - http://codepen.io/pankajparashar/details/towxF/
+    var getMax = function(){
+        return $(document).height() - $(window).height();
+    };
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    var getValue = function(){
+        return $(window).scrollTop();
+    };
 
-    ga('create', 'UA-46746856-1', 'mackenzie.io');
-    ga('send', 'pageview');
-	};
+    if('max' in document.createElement('progress')){
+        var progressBar = $('progress');
+        progressBar.attr({ max: getMax() });
+        $(document).on('scroll', function(){
+            progressBar.attr({ value: getValue() });
+        });
 
-	// Mobile Navigation
-	$(".main_content_nav_m").addClass("hide").before('<div class="nav_small_m"><span class="icon-list"></span></div>');
-	$(".nav_small_m").click(function(){
-		$(".main_content_nav_m").slideToggle(750);
-	});
-	$(window).resize(function(){
-		if(window.innerWidth > 768) {
-			$(".main_content_nav_m").removeAttr("style");
-		}
-	});
+        $(window).resize(function(){
+            progressBar.attr({ max: getMax(), value: getValue() });
+        });
+    }
+    else {
+        var progressBar = $('.progress-bar'),
+            max = getMax(),
+            value, width;
 
-	// SVG Logo Fallback
+        var getWidth = function(){
+            // Calculate width in percentage
+            value = getValue();
+            width = (value/max) * 100;
+            width = width + '%';
+            return width;
+        };
 
-	if(!Modernizr.svg) {
-    $('img[src*="svg"]').attr('src', function() {
-        return $(this).attr('src').replace('.svg', '.png');
-    });
-	}
+        var setWidth = function(){
+            progressBar.css({ width: getWidth() });
+        };
 
-	// Pagination 
-	var $pagination = $('.pagination');
-
-	$pagination
-	.find('.older')
-	.on('click', function(event) {
-
-		event.preventDefault();
-
-		var $this = $(this);
-
-		$.ajax($this.attr('href'))
-		.done(function(data) {
-
-			var $dummy = $('<div></div>').html(data),
-				$postExcerpts = $dummy.find('.post-excerpt'),
-				$older = $dummy.find('.pagination .older');
-
-			$postExcerpts.insertBefore($pagination);
-
-			$this.attr('href', $older.attr('href'));
-
-			if(!$older.length) $this.remove();
-		});
-	});
+        $(document).on('scroll', setWidth);
+        $(window).on('resize', function(){
+            // Need to reset the Max attr
+            max = getMax();
+            setWidth();
+        });
+    }
 });
